f19eca1f4c9541c54f8a9420d75dc6aee7a0c586
bb <btripleb@mail.ru>
2017-09-25 22:57:21 +0600

    Екінші коммит

--- src/BBB/Hello.java
+++ src/BBB/Hello.java
@@ -3,5 +3,7 @@ package BBB;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+        System.out.println("GitHub-пен жұмыс жасап үйрену.");
     }
 }
